7eed5c5a0572222127443d730e596f0a599aaf85
Logan Clark <loganclark.1996@gmail.com>
2025-05-26 23:10:20 -0600

    rework activation functions to return the measurements as well as the altered indices in a dict

--- frames/activation_functions.py
+++ frames/activation_functions.py
@@ -4,20 +4,48 @@ import numpy as np
 def saturation(saturation_value: float, vector: np.ndarray):
     abs_vec = abs(vector)
     sign = np.where(vector == 0, 0, vector / abs_vec)
-    return np.where(abs_vec >= saturation_value, sign * saturation_value, vector)
+    condition = abs_vec >= saturation_value
+    measurements = np.where(condition, sign * saturation_value, vector)
+    indices = {"saturated_indices": condition}
+    return measurements, indices
 
 
 def relu(bias: np.ndarray, vector: np.ndarray):
-    return np.where(vector >= 0, vector, 0) - bias
+    condition = vector < 0
+    unbiased = bias == 0
+    measurements = np.where(condition, 0, vector) - bias
+    indices = {"unbiased_indices": unbiased}
+    return measurements, indices
 
 
 def gate(threshold: float, vector: np.ndarray):
-    return np.where(vector < threshold, 0, vector)
+    condition = vector < threshold
+    measurements = np.where(condition, 0, vector)
+    indices = {
+        "gated_indices": condition,
+    }
+    return measurements, indices
 
 
-def arbitray_range_gate(
+def band_removal_logan(
     lower_threshold: float, upper_threshold: float, vector: np.ndarray
 ):
-    return np.where(
-        lower_threshold <= vector & vector < upper_threshold, lower_threshold, vector
-    )
+    condition = lower_threshold <= vector & vector < upper_threshold
+    measurements = np.where(condition, lower_threshold, vector)
+    indices = {"in_band_indices": condition}
+    return measurements, indices
+
+
+def band_removal_brody(
+    lower_threshold: float, upper_threshold: float, vector: np.ndarray
+):
+    condition1 = lower_threshold <= vector & vector < upper_threshold
+    measurements = np.where(condition1, lower_threshold, vector)
+    condition2 = measurements > lower_threshold
+    measurements = np.where(condition2, vector - lower_threshold, vector)
+    indices = {
+        "below_band_indices": ~(condition1 & condition2),
+        "in_band_indices": condition1,
+        "above_band_indices": condition2,
+    }
+    return measurements, indices
